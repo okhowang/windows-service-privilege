de80b85ddb1fe7e642219ef7d3b1b44eb93a7f4a
okhowang(王沛文) <okhowang@tencent.com>
2019-12-17 19:26:09 +0800

    first commit

---
+++ SvcControl/SvcControl.cpp
@@ -0,0 +1,216 @@
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <aclapi.h>
+#include <stdio.h>
+#include <stdexcept>
+#include <memory>
+#include <sddl.h>
+
+#pragma comment(lib, "advapi32.lib")
+
+TCHAR szCommand[10];
+TCHAR szSvcName[80];
+
+SC_HANDLE schSCManager;
+SC_HANDLE schService;
+
+VOID __stdcall DisplayUsage(void);
+
+VOID __stdcall DoStartSvc(void);
+VOID __stdcall DoUpdateSvcDacl(void);
+VOID __stdcall DoStopSvc(void);
+
+BOOL __stdcall StopDependentServices(void);
+
+class Service {
+    SC_HANDLE handle;
+    friend class ServiceControlManager;
+    Service(SC_HANDLE scm, LPCWSTR name, DWORD access) {
+        handle = OpenService(scm, name, access);
+        if (handle == NULL) {
+            printf("OpenService failed: (%d)\n", GetLastError());
+            throw std::runtime_error("OpenService failed");
+        }
+    }
+    ~Service() {
+        CloseServiceHandle(handle);
+    }
+    void Start() {
+        if (!StartService(handle, 0, NULL)) {
+            printf("StartService failed (%d)\n", GetLastError());
+            throw std::runtime_error("StartService failed");
+        }
+    }
+    SERVICE_STATUS QueryStatus() {
+        SERVICE_STATUS status;
+        if (!QueryServiceStatus(handle, &status)) {
+            printf("QueryServiceStatus failed (%d)\n", GetLastError());
+            throw std::runtime_error("QueryServiceStatus failed");
+        }
+        return status;
+    }
+    void QuerySecurity() {
+        EXPLICIT_ACCESS      ea;
+        SECURITY_DESCRIPTOR  sd;
+        std::unique_ptr<SECURITY_DESCRIPTOR> psd(new SECURITY_DESCRIPTOR);
+        PACL                 pacl = NULL;
+        PACL                 pNewAcl = NULL;
+        BOOL                 bDaclPresent = FALSE;
+        BOOL                 bDaclDefaulted = FALSE;
+        DWORD                dwError = 0;
+        DWORD                dwSize = 0;
+        DWORD                dwBytesNeeded = 0;
+
+        // Get the current security descriptor.
+
+        if (!QueryServiceObjectSecurity(handle,
+            DACL_SECURITY_INFORMATION,
+            psd.get(),           // using NULL does not work on all versions
+            sizeof *psd.get(),
+            &dwBytesNeeded))
+        {
+            if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+            {
+                dwSize = dwBytesNeeded;
+                psd.reset(reinterpret_cast<SECURITY_DESCRIPTOR*>(new uint8_t[dwSize]));
+                if (!QueryServiceObjectSecurity(handle,
+                    DACL_SECURITY_INFORMATION, psd.get(), dwSize, &dwBytesNeeded))
+                {
+                    printf("QueryServiceObjectSecurity second failed (%d)\n", GetLastError());
+                    throw std::runtime_error("QueryServiceObjectSecurity failed");
+                }
+            }
+            else
+            {
+                printf("QueryServiceObjectSecurity failed (%d)\n", GetLastError());
+                throw std::runtime_error("QueryServiceObjectSecurity failed");
+            }
+        }
+
+        // Get the DACL.
+
+        if (!GetSecurityDescriptorDacl(psd.get(), &bDaclPresent, &pacl,
+            &bDaclDefaulted))
+        {
+            printf("GetSecurityDescriptorDacl failed(%d)\n", GetLastError());
+            throw std::runtime_error("GetSecurityDescriptorDacl failed");
+        }
+
+        if (bDaclPresent) {
+            printf("count: %d\n", pacl->AceCount);
+            ULONG count;
+            PEXPLICIT_ACCESS plist;
+            auto ret = GetExplicitEntriesFromAcl(pacl, &count, &plist);
+            if (ret != ERROR_SUCCESS) {
+                printf("GetExplicitEntriesFromAcl failed:%d\n", ret);
+                return;
+            }
+            printf("got %u\n", count);
+            for (auto i = 0; i < count; i++) {
+                printf("%d %x %d %d %d ", plist[i].grfAccessMode, plist[i].grfAccessPermissions, plist[i].grfInheritance,
+                    plist[i].Trustee.TrusteeForm, plist[i].Trustee.TrusteeType);
+                switch (plist[i].Trustee.TrusteeType) {
+                case TRUSTEE_IS_SID:
+                {
+                    SID* sid = reinterpret_cast<SID*>(plist[i].Trustee.ptstrName);
+                    wchar_t name[128], domain[128];
+                    DWORD szName = sizeof name / sizeof name[0];
+                    DWORD szDomain = sizeof domain / sizeof name[0];
+                    SID_NAME_USE use;
+                    LookupAccountSid(NULL, sid, name, &szName, domain, &szDomain, &use);
+                    wprintf(L" %s:%s:%d ", domain, name, use);
+                    if (lstrcmpi(domain, L"NT AUTHORITY") == 0 && (
+                        lstrcmpi(name, L"Authenticated Users") == 0 ||
+                        lstrcmpi(name, L"SYSTEM") == 0
+                        )) {
+                        plist[i].grfAccessPermissions = SERVICE_ALL_ACCESS;
+                        SetEntriesInAcl(count, plist, NULL, &pNewAcl);
+                    }
+                    break;
+                }   
+                default:
+                    printf(" unknown trustee_from ");
+                }
+                printf("\n");
+            }
+
+            if (pNewAcl != NULL) {
+                if (!InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION)) {
+                    printf("InitializeSecurityDescriptor failed(%d)\n", GetLastError());
+                    throw std::runtime_error("InitializeSecurityDescriptor failed");
+                }
+                if (!SetSecurityDescriptorDacl(&sd, TRUE, pNewAcl, FALSE)) {
+                    printf("SetSecurityDescriptorDacl failed(%d)\n", GetLastError());
+                    throw std::runtime_error("SetSecurityDescriptorDacl failed");
+                }
+                if (!SetServiceObjectSecurity(handle,
+                    DACL_SECURITY_INFORMATION, &sd))
+                {
+                    printf("SetServiceObjectSecurity failed(%d)\n", GetLastError());
+                    throw std::runtime_error("SetServiceObjectSecurity failed");
+                }
+                else printf("Service DACL updated successfully\n");
+            }
+        }
+        else {
+            printf("no dacl present\n");
+        }
+    }
+public:
+};
+class ServiceControlManager {
+    SC_HANDLE handle;
+public:
+    ServiceControlManager(const ServiceControlManager&) = delete;
+    ServiceControlManager(ServiceControlManager&&) = delete;
+    ServiceControlManager& operator =(const ServiceControlManager) = delete;
+    ServiceControlManager() {
+        handle = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+        if (handle == NULL) {
+            printf("OpenSCManager failed (%d)\n", GetLastError());
+            throw std::runtime_error("OpenSCManager failed");
+        }
+    }
+    ~ServiceControlManager() {
+        CloseServiceHandle(handle);
+    }
+
+    void Start(LPCWSTR serviceName) {
+        Service service(handle, serviceName, SERVICE_START | SERVICE_QUERY_STATUS);
+        auto status = service.QueryStatus();
+        if (status.dwCurrentState != SERVICE_STOPPED && status.dwCurrentState != SERVICE_STOP_PENDING) {
+            printf("Cannot start the service because it is already running\n");
+            return;
+        }
+        service.Start();
+        for (status = service.QueryStatus(); status.dwCurrentState == SERVICE_START_PENDING; Sleep(1000));
+        if (status.dwCurrentState == SERVICE_RUNNING)
+        {
+            printf("Service started successfully.\n");
+        }
+        else
+        {
+            printf("Service not started. \n");
+            printf("  Current State: %d\n", status.dwCurrentState);
+            printf("  Exit Code: %d\n", status.dwWin32ExitCode);
+            printf("  Check Point: %d\n", status.dwCheckPoint);
+            printf("  Wait Hint: %d\n", status.dwWaitHint);
+        }
+    }
+
+    void QueryConfig(LPCWSTR serviceName) {
+        Service service(handle, serviceName, READ_CONTROL| WRITE_DAC);
+        service.QuerySecurity();
+    }
+};
+
+int main()
+{
+    try {
+        ServiceControlManager().QueryConfig(L"SharedAccess");
+    }
+    catch (std::exception &e) {
+        printf("exception: %s\n", e.what());
+    }
+}
